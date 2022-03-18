3839dfddaa008fd1791f9cca85c1df4425c6213f
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2022-03-18 21:36:28 +0000

    Connexion SQL to JAVA

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,22 @@
 package Main;
 
+import bdd.mySQL;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Test");
+
+        mySQL bdd = new mySQL();
+        String[] test;
+        //bdd.insert1("Insert into client (name,surname) Values(\"Victoria\",\"Robert\")");
+        test = bdd.multipleSelect("SELECT name FROM client");
+
+        for(String i : test) System.out.println(i);
+
+        //System.out.println(test[2]);
+
     }
+
+
 }
