ff5e9f3223e1acf31e8af0fe5025a19713832679
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2022-03-19 23:51:04 +0000

    Back of loggin module + Hash password

--- src/bdd/mySQL.java
+++ src/bdd/mySQL.java
@@ -5,6 +5,41 @@ import java.util.ArrayList;
 
 public class mySQL {
 
+    public String select(String query) {
+        // Variables
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/filmsproject";
+            String user = "root";
+            String password = "27Maxecq-";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            // more processing here
+            // ...
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                // Instructions
+                return rs.getString(1);
+
+            }
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return "";
+    }
+
     public String[] multipleSelect(String query) {
         // Variables
         ArrayList<String> output = new ArrayList<>();
