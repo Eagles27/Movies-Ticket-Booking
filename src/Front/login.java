bdb9ade63d8ad22bcb1aa06c8500c47d6e5fd91e
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2022-03-25 15:53:12 +0000

    Rajout

---
+++ src/Front/login.java
@@ -0,0 +1,10 @@
+package Front;
+
+import javax.swing.*;
+
+public class login {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+    private JButton button2;
+}
