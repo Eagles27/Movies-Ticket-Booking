95997d45226f7a74ddb6490a240f2b2dc97b371e
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2022-03-31 23:09:16 +0100

    add connected label on SelectDatePage + fill fields with BDD

--- src/Back/ListMovies.java
+++ src/Back/ListMovies.java
@@ -34,4 +34,8 @@ public class ListMovies {
     public ArrayList<String> getListMoviesReleased() {
         return listMoviesReleased;
     }
+
+    public String getMovieName(int index){
+        return listMoviesName.get(index);
+    }
 }
--- src/Front/SelectDate.java
+++ src/Front/SelectDate.java
@@ -1,12 +1,13 @@
 package Front;
 
 import Back.ClientInfo;
+import Back.ListMovies;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SelectDate extends JFrame{
+public class SelectDate extends JFrame {
     private JPanel SelectDataPane;
     private JButton proceedToPaymentButton;
     private JRadioButton heureSéance1RadioButton;
@@ -15,10 +16,14 @@ public class SelectDate extends JFrame{
     private JRadioButton heureSéance4RadioButton;
     private JRadioButton heureSéance5RadioButton;
     private JButton button1;
+    private JLabel JLabelTitle;
+    private JLabel JLabelNumberTicket;
+    private JLabel JLabelName;
+    private JLabel JLabelMember;
+    private JLabel JLabelDate;
 
 
-
-    public SelectDate(int movie, int numberOfTicket, String date, int age, int[] index){
+    public SelectDate(int movie, int numberOfTicket, String date, int age, int[] index) {
         JFrame window = new JFrame();
         window.setContentPane(SelectDataPane);
         window.setTitle("SelectDate");
@@ -26,17 +31,23 @@ public class SelectDate extends JFrame{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
 
+        //Backup Movie
+        ListMovies movies = new ListMovies();
+        JLabelTitle.setText(movies.getMovieName(movie));
+        JLabelNumberTicket.setText(String.valueOf(numberOfTicket));
+        JLabelDate.setText("Available film sessions on " + date + " :");
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 window.dispose();
-                new BuyPage(movie,numberOfTicket,date,age,index);
+                new BuyPage(movie, numberOfTicket, date, age, index);
 
             }
         });
     }
 
-    public SelectDate(int movie, int numberOfTicket, String date, int age, int[] index, int id){
+    public SelectDate(int movie, int numberOfTicket, String date, int age, int[] index, int id) {
         JFrame window = new JFrame();
         window.setContentPane(SelectDataPane);
         window.setTitle("SelectDate");
@@ -45,15 +56,24 @@ public class SelectDate extends JFrame{
         window.setVisible(true);
 
         ClientInfo user = new ClientInfo(id);
+        JLabelMember.setText(user.getMember());
+        JLabelName.setText(user.getName() + user.getSurname());
+        //Backup Movie
+        ListMovies movies = new ListMovies();
+        JLabelTitle.setText(movies.getMovieName(movie));
+        JLabelNumberTicket.setText(String.valueOf(numberOfTicket));
+        JLabelDate.setText("Available film sessions on " + date + " :");
+
 
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 window.dispose();
-                new BuyPage(user.getMail(),movie,numberOfTicket,date,age,index);
+                new BuyPage(user.getMail(), movie, numberOfTicket, date, age, index);
 
             }
         });
     }
+
 
 }
